c8a839cb592376fcb622533fe83b041c0c3f4865
Valentyn Chubukin <val@valch.name>
2024-07-17 00:03:37 +0200

    Sets & enums

--- 3.playground/Contents.swift
+++ 3.playground/Contents.swift
@@ -59,3 +59,77 @@ print(cities.sorted())
 let presidents = ["Bush", "Obama", "Trump", "Biden"]
 let reversedPresidents = presidents.reversed()
 print(reversedPresidents)
+
+var employee = ["Taylor Swift", "Singer", "Nashville"]
+
+print("Name: \(employee[0])")
+employee.remove(at: 1)
+print("Job title: \(employee[1])")
+//print("Location: \(employee[2])")
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+print(employee2["name", default: "Unknown"])
+print(employee2["job", default: "Unknown"])
+print(employee2["location", default: "Unknown"])
+print(employee2["password"])
+print(employee2["status"])
+print(employee2["manager"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2021: "Tokyo"
+]
+
+print(olympics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin"
+
+print(heights.count)
+heights.removeAll()
+print(heights.count)
+
+
+let people2 = Set(["Denzel Washington", "Tom Cruise", "Nicolas Cage", "Samuel L Jackson"])
+print(people2)
+
+var people = Set<String>()
+people.insert("Denzel Washington")
+people.insert("Tom Cruise")
+people.insert("Nicolas Cage")
+people.insert("Samuel L Jackson")
+print(people)
+
+
+//enum
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = Weekday.tuesday
+day = .friday
+
