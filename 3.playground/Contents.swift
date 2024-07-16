cfe52da68c8087bd5ada27279f5b19339ea354c9
Valentyn Chubukin <val@valch.name>
2024-07-16 23:04:43 +0200

    Arrays

--- 3.playground/Contents.swift
+++ 3.playground/Contents.swift
@@ -1,3 +1,61 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+var beatles = ["John", "Paul", "George", "Ringo"]
+var numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
+
+beatles.append("Adrian")
+beatles.append("Allen")
+beatles.append("Adrian")
+beatles.append("Novall")
+beatles.append("Vivian")
+print(beatles)
+
+//temperatures.append("Chris")
+let firstBeatle = beatles[0]
+let firstNumber = numbers[0]
+//let notAllowed = firstBeatle + firstNumber
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(80)
+scores.append(85)
+print(scores[1])
+
+//var albums = Array<String>()
+//albums.append("Folklore")
+//albums.append("Fearless")
+//albums.append("Red")
+
+//var albums = [String]()
+var albums = ["Folklore"]
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("Red")
+
+print("Albums number is : \(albums.count)")
+
+
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+
+characters.remove(at: 2)
+print(characters.count)
+
+characters.removeAll()
+print(characters.count)
+
+let bondMovies = ["Casino Royale", "Spectre", "No Time To Die"]
+print(bondMovies.contains("Frozen"))
+
+let cities = ["London", "Tokyo", "Rome", "Budapest"]
+print(cities.sorted())
+
+let presidents = ["Bush", "Obama", "Trump", "Biden"]
+let reversedPresidents = presidents.reversed()
+print(reversedPresidents)
