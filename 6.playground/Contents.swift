e8fb56c01a60f65857842c53ce883b70546bbb59
Valentyn Chubukin <val@valch.name>
2024-07-18 03:05:01 +0200

    for loop

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -1,3 +1,43 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+
+for i in 1...12 {
+    //print("5 x \(i) is \(5 * i)")
+    print("The \(i) times table")
+    
+    for j in 1...12 {
+        print("   \(j) x \(i) is \(j * i)")
+    }
+    
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+for i in 1..<5 {
+    print("Counting from 1 up to 5: \(i)")
+}
+
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+}
+
+print(lyric)
+
+var numbers = [1, 2, 3, 4, 5, 6]
+for number in numbers {
+    if number.isMultiple(of: 2) {
+        print(number)
+    }
+}
+
