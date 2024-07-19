daf5312568cfffee806c41e1771550427a211764
Valentyn Chubukin <val@valch.name>
2024-07-19 16:06:19 +0200

    skip loop items with break and continue

--- 6.playground/Contents.swift
+++ 6.playground/Contents.swift
@@ -98,3 +98,29 @@ while averageScore < 15.0 {
     averageScore += 2.5
     print("The average score is \(averageScore)")
 }
+
+// skip item in a loop
+let filenames = ["me.jpg", "work.txt", "sophie.jpg"]
+
+for filename in filenames {
+    if filename.hasSuffix(".jpg") == false {
+        continue
+    }
+    print("Found picture: \(filename)")
+}
+
+// to brake when the issue
+let number1 = 4
+let number2 = 14
+var multiples = [Int]()
+
+for i in 1...100_000 {
+    if i.isMultiple(of: number1) && i.isMultiple(of: number2) {
+        multiples.append(i)
+
+        if multiples.count == 10 {
+            break
+        }
+    }
+}
+print(multiples)
