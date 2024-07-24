c95bffff85491f245d9f128d4680687fc3ad05d9
Valentyn Chubukin <val@valch.name>
2024-07-24 18:12:02 +0200

    struct && mutating func

---
+++ 10.playground/Contents.swift
@@ -0,0 +1,44 @@
+import Cocoa
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    //let name: String = "Anonymous"
+    let name: String
+    var vacationRemaining: Int = 14
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vacation")
+            print("Days remaning \(vacationRemaining) ")
+        } else {
+            print("Oops! No days remaning.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
+//var archer = Employee(vacationRemaining: 14)
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+let kane = Employee(name: "Lana Kane")
+let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
