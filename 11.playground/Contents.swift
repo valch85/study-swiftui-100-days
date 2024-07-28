a10559db03693d1c8493c6e507a549abcad5f458
Valentyn Chubukin <val@valch.name>
2024-07-29 01:12:54 +0200

    limit access to internal data

--- 11.playground/Contents.swift
+++ 11.playground/Contents.swift
@@ -1,4 +1,29 @@
 import Cocoa
 
 //Structs, part two
+// limit access to internal data
+struct BankAccount {
+    var funds = 0
 
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
