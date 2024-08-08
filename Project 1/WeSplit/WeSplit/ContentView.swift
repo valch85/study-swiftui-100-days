10ff17bafd01118c9989b11fd3deb18023d36caf
Valentyn Chubukin <val@valch.name>
2024-08-08 22:07:25 +0200

    Creating a form

--- Project 1/WeSplit/WeSplit/ContentView.swift
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Text("Hello, world!")
+            }
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
+            Section {
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+                Text("Hello, world!")
+            }
         }
-        .padding()
     }
 }
 
