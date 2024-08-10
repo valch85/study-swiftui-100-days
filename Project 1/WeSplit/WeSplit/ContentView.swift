6906b0f39633e6149637fab606266351413338e3
Valentyn Chubukin <val@valch.name>
2024-08-11 00:18:00 +0200

    TextField

--- Project 1/WeSplit/WeSplit/ContentView.swift
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -8,6 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
+    }
+    
+    /*
     let students = ["Harry", "Hermione", "Ron"]
     @State private var selectedStudent = "Harry"
     @State private var name = ""
@@ -42,6 +61,7 @@ struct ContentView: View {
             }
         }
     }
+*/
 }
 
 #Preview {
