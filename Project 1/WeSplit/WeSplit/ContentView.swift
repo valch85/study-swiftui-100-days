8907b279473cb3d4fa33e049aacf43608b2c0ca8
Valentyn Chubukin <val@valch.name>
2024-08-13 23:20:33 +0200

    added tasks; done 1st task

--- Project 1/WeSplit/WeSplit/ContentView.swift
+++ Project 1/WeSplit/WeSplit/ContentView.swift
@@ -4,6 +4,11 @@
 //
 //  Created by Valentyn Chubukin on 08.08.2024.
 //
+// Tasks
+
+// - Add a header to the third section, saying “Amount per person”
+// - Add another section showing the total amount for the check – i.e., the original amount plus tip value, without dividing by the number of people.
+// - Change the tip percentage picker to show a new screen rather than using a segmented control, and give it a wider range of options – everything from 0% to 100%. Tip: use the range 0..<101 for your range rather than a fixed array.
 
 import SwiftUI
 
@@ -50,7 +55,7 @@ struct ContentView: View {
                     }
                     .pickerStyle(.segmented) //make choose option like a buttons
                 }
-                Section("Amount for each person") {
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
