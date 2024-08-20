c5bf93251d0ace320b4cdedaa3934d8d140072d8
Valentyn Chubukin <val@valch.name>
2024-08-20 21:42:51 +0100

    Colors and frames

--- Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,16 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            Spacer()
+            Color(red: 1, green: 0.8, blue: 0)
             Text("Hello, world!")
             Text("This is inside a stack")
             Spacer()
             Spacer()
         }
+        .ignoresSafeArea()
+        //.background(.blue)
         ZStack(alignment: .top) {
-            Text("Hello, world!")
+            Color.red
+                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
             Text("This is inside a stack")
+                //.background(.red)
         }
+        .ignoresSafeArea()
     }
     
 }
