004140f99afd40e577818a146d352d2302eb1803
Valentyn Chubukin <val@valch.name>
2024-08-30 20:37:38 +0200

    What is behind the main SwiftUI view?

---
+++ Project 3/ViewsAndModifiers1/ViewsAndModifiers1/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers1
+//
+//  Created by Valentyn Chubukin on 30/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project 3/ViewsAndModifiers1/ViewsAndModifiers1/ViewsAndModifiers1App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers1App.swift
+//  ViewsAndModifiers1
+//
+//  Created by Valentyn Chubukin on 30/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiers1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
