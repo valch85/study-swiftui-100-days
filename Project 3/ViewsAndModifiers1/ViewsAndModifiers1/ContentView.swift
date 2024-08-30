43369c0dda894efb363e53e0056d833f5054a2a3
Valentyn Chubukin <val@valch.name>
2024-08-30 21:47:39 +0200

    Views as properties

--- Project 3/ViewsAndModifiers1/ViewsAndModifiers1/ContentView.swift
+++ Project 3/ViewsAndModifiers1/ViewsAndModifiers1/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    // == Conditional modifiers ==
     @State private var useRedText = false
     
+    // == Views as properties ==
+    var motto1: some View {
+        Text("Draco dormiens")
+    }
+    
+    let motto2 = Text("nunquam titillandus")
+    
+    var spells1: some View {
+        VStack {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    // you can also send back a Group
+    var spells2: some View {
+        Group {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    // third option is to add the @ViewBuilder attribute yourself
+    @ViewBuilder var spells3: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
+    
     var body: some View {
+        // == Views as properties ==
+        VStack {
+            motto1
+                .foregroundStyle(.red)
+            motto2
+                .foregroundStyle(.blue)
+                }
+        // == Environment modifiers ==
         VStack {
             Text("Gryffindor")
                 .font(.largeTitle)
@@ -30,6 +65,7 @@ struct ContentView: View {
         .blur(radius: 5)
         .padding()
         
+        // == Conditional modifiers ==
         // 1 create one button and recolor it when needed - more efficient way
         Button("Hello, world!") {
             useRedText.toggle()
@@ -48,6 +84,7 @@ struct ContentView: View {
             .foregroundStyle(.yellow)
         }
         
+        // == Why modifier order matters + What is behind the main SwiftUI view? ==
         Button("Hello, world!") {
             useRedText.toggle()
             //print(type(of: self.body))
