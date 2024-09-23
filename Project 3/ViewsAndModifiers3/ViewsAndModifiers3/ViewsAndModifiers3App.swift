72d66e04f22854ec6f1a0fe88876db72c8d1bf38
Valentyn Chubukin <val@valch.name>
2024-09-23 22:59:04 +0200

    project 3 - challenge 3

---
+++ Project 3/ViewsAndModifiers3/ViewsAndModifiers3/ViewsAndModifiers3App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers3App.swift
+//  ViewsAndModifiers3
+//
+//  Created by Valentyn Chubukin on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiers3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project 3/ViewsAndModifiers3/ViewsAndModifiers3Tests/ViewsAndModifiers3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers3Tests.swift
+//  ViewsAndModifiers3Tests
+//
+//  Created by Valentyn Chubukin on 23/09/2024.
+//
+
+import Testing
+@testable import ViewsAndModifiers3
+
+struct ViewsAndModifiers3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
