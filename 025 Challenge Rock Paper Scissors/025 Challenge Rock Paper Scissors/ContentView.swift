64cfacaaac83458443a7a181ad297ac6f2bdce39
Valentyn Chubukin <val@valch.name>
2025-02-02 16:57:12 +0100

    025 add: full logic for the result comparisont; add proper text prompts; count rounds ammount; restart logic; add info to README

--- 025 Challenge Rock Paper Scissors/025 Challenge Rock Paper Scissors/ContentView.swift
+++ 025 Challenge Rock Paper Scissors/025 Challenge Rock Paper Scissors/ContentView.swift
@@ -28,8 +28,12 @@ struct CustomButtonStyle: ButtonStyle {
 
 struct ContentView: View {
     @State private var varAnswer = ["Rock", "Paper", "Scissors"]
-    @State private var scoreAmount = 0
     @State private var pcAnswer = Int.random(in: 0...2)
+    
+    @State private var scoreAmount = 0
+    @State private var gamesAmount = 1
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
@@ -41,13 +45,16 @@ struct ContentView: View {
                 .ignoresSafeArea()
             VStack {
                 Spacer()
-                Text("Game Rock, Paper or Scissors:")
+                Text("Game Rock, Paper or Scissors. 8 rounds.")
                     .font(.largeTitle.weight(.bold))
                     .foregroundStyle(.white)
                     .shadow(color: .black.opacity(0.99), radius: 4.5, x: -10, y: 15)
                 VStack(spacing: 25) {
                     VStack {
-                        Text("PC answer is: \(varAnswer[pcAnswer])" )
+                        //Text("PC answer is: \(varAnswer[pcAnswer])" )
+                        Text("Round # \(gamesAmount)")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
                         Text("Choose yours answer:")
                             .foregroundStyle(.secondary)
                             .font(.subheadline.weight(.heavy))
@@ -75,16 +82,31 @@ struct ContentView: View {
                 
                 Spacer()
                 Spacer()
+                /*
                 Text("Score: \(scoreAmount)")
                     .foregroundStyle(.white)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
                     .shadow(color: .black.opacity(0.99), radius: 4.5, x: -10, y: 15)
                 Spacer()
+                */
             }
             .padding()
             
             
             
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            if gamesAmount > 8 {
+                Button("Restart", action: newGame)
+            } else {
+                Button("Continue", action: askQuestion)
+            }
+        } message: {
+            if gamesAmount > 8 {
+                Text("Your score is \(scoreAmount). You reach 8 games. Restart the game to continue.")
+            } else {
+                //Text("Your score is \(scoreAmount)")
+            }
         }
         
         
@@ -92,37 +114,66 @@ struct ContentView: View {
 
 
     func answerTap(_ userAnswer: String) {
+        gamesAmount += 1
         switch(varAnswer[pcAnswer], userAnswer) {
             case ("Rock", "Rock"):
+                scoreTitle = "Draw"
                 scoreAmount = scoreAmount + 0
+                showingScore = true
                 break
             case ("Paper", "Paper"):
+                scoreTitle = "Draw"
                 scoreAmount = scoreAmount + 0
+                showingScore = true
                 break
             case ("Scissors", "Scissors"):
+                scoreTitle = "Draw"
                 scoreAmount = scoreAmount + 0
+                showingScore = true
                 break
             case ("Rock", "Paper"):
-                scoreAmount = scoreAmount - 1
+                scoreTitle = "Win. You choose Papre. Compuret choose \(varAnswer[pcAnswer])."
+                scoreAmount = scoreAmount + 1
+                showingScore = true
                 break
             case ("Rock", "Scissors"):
+                scoreTitle = "Win. You choose Scissors. Compuret choose \(varAnswer[pcAnswer])."
                 scoreAmount = scoreAmount + 1
+                showingScore = true
                 break
             case ("Paper", "Rock"):
+                scoreTitle = "Loss. You choose Rock. Compuret choose \(varAnswer[pcAnswer])."
                 scoreAmount = scoreAmount - 1
+                showingScore = true
                 break
             case ("Scissors", "Rock"):
+                scoreTitle = "Win. You choose Rock. Compuret choose \(varAnswer[pcAnswer])."
                 scoreAmount = scoreAmount + 1
+                showingScore = true
                 break
             case ("Scissors", "Paper"):
+                scoreTitle = "Loss. You choose Paper. Compuret choose \(varAnswer[pcAnswer])."
                 scoreAmount = scoreAmount - 1
+                showingScore = true
                 break
-            case ("Scissors", "Rock"):
+            case ("Paper", "Scissors"):
+                scoreTitle = "Win. You choose Scissors. Compuret choose \(varAnswer[pcAnswer])."
                 scoreAmount = scoreAmount + 1
+                showingScore = true
                 break
             default:
                 break
         }
+    }
+    
+    func askQuestion() {
+        pcAnswer = Int.random(in: 0...2)
+    }
+    
+    func newGame() {
+        askQuestion()
+        gamesAmount = 1
+        scoreAmount = 0
     }
 
 }
