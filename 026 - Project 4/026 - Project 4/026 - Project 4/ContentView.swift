63e1ec7e4b57c50d1fe1acd940e49fe1d46b539d
Valentyn Chubukin <val@valch.name>
2025-02-09 01:33:58 +0100

    init Project 4

---
+++ 026 - Project 4/026 - Project 4/026 - Project 4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  026 - Project 4
+//
+//  Created by Valentyn Chubukin on 09/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
