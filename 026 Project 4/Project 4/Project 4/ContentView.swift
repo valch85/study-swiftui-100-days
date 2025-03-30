9e97c9c3da0b9e127fa2e1a2fffaca1b9f99fc42
Valentyn Chubukin <val@valch.name>
2025-03-30 17:00:54 +0200

    challenge for project 4

--- 026 Project 4/Project 4/Project 4/ContentView.swift
+++ 026 Project 4/Project 4/Project 4/ContentView.swift
@@ -54,6 +54,7 @@ struct ContentView: View {
                     //Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
                 }
                 
+                
             }
             //Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
             //.bold()
