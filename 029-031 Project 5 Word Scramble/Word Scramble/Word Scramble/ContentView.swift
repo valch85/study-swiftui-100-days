5d6a42fc1de29e76d963b02827ace2bd46437e64
Valentyn Chubukin <val@valch.name>
2025-04-11 17:37:42 +0200

    Introducing List

--- 029-031 Project 5 Word Scramble/Word Scramble/Word Scramble/ContentView.swift
+++ 029-031 Project 5 Word Scramble/Word Scramble/Word Scramble/ContentView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
+        
+        // List mixed content
         List {
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
+            Section("Section 1") {
+                Text("Static Row 1")
+                Text("Static Row 2")
+            }
+            Section("Section 2") {
+                ForEach(0..<2) {
+                    Text("Dynamic Row \($0)")
+                }
+            }
+            Section("Section 3") {
+                Text("Static Row 3")
+                Text("Static Row 4")
+            }
         }
-        .padding()
+        .listStyle(.grouped)
+        
+        // List just dynamic content
+        List (0..<2) {
+                    Text("Dynamic Row \($0)")
+        }
+        
+        // List from array dynamic
+        List (people, id: \.self) {
+                    Text($0)
+        }
+        
+        // List from array mixed with ForEach
+        List {
+            Text("Static Row")
+            ForEach (people, id: \.self) {
+                        Text($0)
+            }
+            Text("Static Row")
+        }
+        
     }
 }
 
