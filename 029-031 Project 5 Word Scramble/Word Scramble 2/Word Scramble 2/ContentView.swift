cabd86076746d5cfadeab96dee218fbed8a73219
Valentyn Chubukin <val@valch.name>
2025-04-15 00:24:49 +0200

    Project 5. Validating words with UITextChecker.

--- 029-031 Project 5 Word Scramble/Word Scramble 2/Word Scramble 2/ContentView.swift
+++ 029-031 Project 5 Word Scramble/Word Scramble 2/Word Scramble 2/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-// Функция, которая переводит строку в массив символов
+// Func that change string to array 
 func stringToArray(_ input: String) -> [String] {
     return input.map { String($0) }
 }
@@ -16,6 +16,10 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
         NavigationStack {
@@ -37,6 +41,11 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK") { }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
@@ -48,6 +57,21 @@ struct ContentView: View {
         guard answer.count > 0 else { return }
 
         // extra validation to come
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
         withAnimation {
             usedWords.insert(answer, at: 0)
@@ -67,6 +91,39 @@ struct ContentView: View {
         
         fatalError("Couldn't load start.txt from bundle.")
     }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
