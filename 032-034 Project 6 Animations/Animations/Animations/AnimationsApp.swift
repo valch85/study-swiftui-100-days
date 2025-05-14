58c51c890bbccecc94819238a3224cbab5106cd6
valch85 <val@valch.name>
2025-05-14 09:52:37 +0200

    changes on 025-031

---
+++ 032-034 Project 6 Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by val on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 032-034 Project 6 Animations/Animations/AnimationsTests/AnimationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsTests.swift
+//  AnimationsTests
+//
+//  Created by val on 14/05/2025.
+//
+
+import Testing
+@testable import Animations
+
+struct AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
