b3f2f27ae6b8844fad12e42fbcd117db53fe70ea
valch85 <val@valch.name>
2025-05-18 00:47:07 +0200

    Animating bindings

--- 032-034 Project 6 Animations/Animations/Animations/ContentView.swift
+++ 032-034 Project 6 Animations/Animations/Animations/ContentView.swift
@@ -11,26 +11,48 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
-        Button("Tap Me") {
-//            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeOut(duration: 1)
+        print(animationAmount)
+        return VStack {
+            // Animating bindings
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
+            Spacer()
+            
+            // Customizing animations in SwiftUI
+            Button("Tap Me") {
+                //            animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
                         .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+                        value: animationAmount
+                    )
+            )
+            .onAppear {
+                animationAmount = 2
+            }
         }
     }
 }
