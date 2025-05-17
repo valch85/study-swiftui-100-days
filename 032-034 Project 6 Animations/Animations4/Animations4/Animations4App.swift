137a339d9d4ad9c29cf5d117ea7f441633134ff0
valch85 <val@valch.name>
2025-05-18 01:36:03 +0200

    init Animating gestures

---
+++ 032-034 Project 6 Animations/Animations4/Animations4/Animations4App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations4App.swift
+//  Animations4
+//
+//  Created by val on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Animations4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 032-034 Project 6 Animations/Animations4/Animations4Tests/Animations4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Animations4Tests.swift
+//  Animations4Tests
+//
+//  Created by val on 18/05/2025.
+//
+
+import Testing
+@testable import Animations4
+
+struct Animations4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
