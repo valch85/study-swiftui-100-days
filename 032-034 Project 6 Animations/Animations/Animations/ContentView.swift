2819d685d068d6032c7ef0c1b037373673ad740c
valch85 <val@valch.name>
2025-05-18 00:07:09 +0200

    Project 6 Animations repeatForever

--- 032-034 Project 6 Animations/Animations/Animations/ContentView.swift
+++ 032-034 Project 6 Animations/Animations/Animations/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+
     var body: some View {
-        Button("Tap me") {
+        Button("Tap Me") {
             animationAmount += 1
         }
         .padding(50)
@@ -18,8 +19,11 @@ struct ContentView: View {
         .foregroundStyle(.white)
         .clipShape(.circle)
         .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1 ) * 3)
-        .animation(.default, value: animationAmount)
+        .animation(
+            .easeInOut(duration: 1)
+                .repeatForever(autoreverses: true),
+            value: animationAmount
+        )
     }
 }
 
