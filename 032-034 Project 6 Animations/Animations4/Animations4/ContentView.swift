9796081ec1464a5a41d036a727484cfed5a30ecb
valch85 <val@valch.name>
2025-05-18 11:00:27 +0200

    Animating gestures

--- 032-034 Project 6 Animations/Animations4/Animations4/ContentView.swift
+++ 032-034 Project 6 Animations/Animations4/Animations4/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        LinearGradient(colors: [.blue, .yellow], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            //.animation(.bouncy, value: dragAmount)
     }
 }
 
