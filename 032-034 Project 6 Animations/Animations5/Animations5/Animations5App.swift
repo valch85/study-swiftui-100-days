6261ce182aca5be33e11f52e194e8f94ab0f0d35
valch85 <val@valch.name>
2025-05-18 12:40:59 +0200

    033 Project 6 Animation 5 letters pulled like a snake

---
+++ 032-034 Project 6 Animations/Animations5/Animations5/Animations5App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations5App.swift
+//  Animations5
+//
+//  Created by val on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Animations5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 032-034 Project 6 Animations/Animations5/Animations5Tests/Animations5Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Animations5Tests.swift
+//  Animations5Tests
+//
+//  Created by val on 18/05/2025.
+//
+
+import Testing
+@testable import Animations5
+
+struct Animations5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
