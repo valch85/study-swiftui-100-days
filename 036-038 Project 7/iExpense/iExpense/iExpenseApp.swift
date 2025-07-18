d95911aac5be66179a6b830e5ce87f812c0f4df6
valch85 <val@valch.name>
2025-07-19 00:23:40 +0200

    036-038 Project 7. init iExpense.

---
+++ 036-038 Project 7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by val on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
